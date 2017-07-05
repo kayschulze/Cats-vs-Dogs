8060b18fdbe50d34c53fea0ce8e3973d6a60b7c1
Kim Schulze and Victor Puentes <kim@>
2017-07-05 13:55:20 -0700

    Add dog and cat picture using prepend and append functions of JQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
   $("button#cat-meow").click(function() {
-    $("ul").prepend("<li>Meow</li>");
+    $("ul.cat").prepend("<li>Meow</li>");
+    $("ul.cat").append("<img src='img/catmeow.jpeg'>");
   });
-
   $("button#dog-bark").click(function() {
-    $("ul").prepend("<li>Bark</li>");
+    $("ul.dog").prepend("<li>Bark</li>");
+    $("ul.dog").append("<img src='img/dogbark.jpeg'>");
   });
 });
